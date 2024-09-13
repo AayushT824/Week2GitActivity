19deccc424bb7c9190c793a9d4e2fa8395ee7b56
greatestblueheron <s.xu.nature@gmail.com>
2024-09-13 14:47:06 -0400

    print hello message

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ const result_add = calc.add(2, 3);
 const result_subtract = calc.subtract(2, 3);
 console.log(`result_add = ${result_add}`);
 console.log(`result_subtract = ${result_subtract}`);
-
+calc.hello();
